d0839dc992660ea1923b53395e064cd302265a06
Rinkal <email@example.com>
2023-09-30 21:14:49 +0100

    added code for detail page

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const Product = ({ product: { image, name, price } }) => {
+const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
+        <Link href={`/product/${slug.current}`}>
           <div className="card">
           <img 
             src={urlFor(image && image[0])}
@@ -16,12 +17,12 @@ const Product = ({ product: { image, name, price } }) => {
              
               <div className="card-body">
                 <h5 className="card-title">{name}</h5>
-                <p className="product-price">${price}</p>
+                <p className="product-price">£{price}</p>
 
                
               </div>
             </div>
-     
+            </Link>
     </div>
   )
 }
