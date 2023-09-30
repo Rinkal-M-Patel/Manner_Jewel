9e9e6525df14627d14a17b6407fb0a6aefe59318
Rinkal <email@example.com>
2023-09-30 18:05:30 +0100

    solve packages error and install new one

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Home = () => {
+
+const Product = () => {
   return (
     <div>
-   <h1>index</h1>
-    
+      
     </div>
   )
 }
 
-
-export default Home
+export default Product
