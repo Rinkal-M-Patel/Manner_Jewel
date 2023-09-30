8e23fafa5abd3a799f4565fdbd6911ac424bf64a
Rinkal <email@example.com>
2023-09-30 20:43:25 +0100

    fetch data from sanity

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 
-const Product = () => {
+import { urlFor } from '../lib/client';
+
+const Product = ({ product: { image, name, price } }) => {
   return (
     <div>
-      
+          <div className="card">
+          <img 
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+             
+              <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="product-price">${price}</p>
+
+               
+              </div>
+            </div>
+     
     </div>
   )
 }
