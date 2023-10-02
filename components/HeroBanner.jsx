b04c0f81f1ad4b4a168a0757a4c3181e0059c894
Etinago <124711424+Etinago@users.noreply.github.com>
2023-10-02 23:01:17 +0100

    added Footer and HeroBanner components

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,21 @@
+// HeroBanner
+
+import React from 'react';
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Manner, jewellery made For, By You</h1>
+            <p>Discover our latest products and deals.</p>
+            <button>See more</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
