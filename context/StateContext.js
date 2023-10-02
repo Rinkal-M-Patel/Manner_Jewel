c3a828cc3476a3ef14384076011cd01aa41cce5c
Rinkal <email@example.com>
2023-10-02 21:50:15 +0100

    add cdoe to subtotal

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({children}) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
+    let foundProduct;
+    let index;
+
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
         setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
@@ -36,6 +39,26 @@ export const StateContext = ({children}) => {
         
       } 
 
+
+      
+  const toggleCartItemQuanitity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id)
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if(value === 'inc') {
+      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+    } else if(value === 'dec') {
+      if (foundProduct.quantity > 1) {
+        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+      }
+    }
+  }
+
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
       }
@@ -60,6 +83,7 @@ export const StateContext = ({children}) => {
         incQty,
         decQty,
         onAdd,
+        toggleCartItemQuanitity,
         setCartItems,
         setTotalPrice,
         setTotalQuantities
