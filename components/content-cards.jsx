c2a4db60c93a52c090d963999d53380ff2c3f912
unknown <diloch111@gmail.com>
2023-10-03 21:06:31 +0100

    export the jewelryCard module

--- components/content-cards.jsx
+++ components/content-cards.jsx
@@ -6,14 +6,16 @@ const JewelryCard = ({ jewelryData, addToCart }) =>
 {
     return (
         <div className="card">
-            <img src={jewelryData.imageURL} alt={jewelryData.name} className="card-img-top" />;
+            <img src={jewelryData.imageURL} alt={jewelryData.name} className="card-img-top" />
             <div className="card-body"> 
-            <h5 className="card-title">{jewelryData.name}</h5>;
-            <p className="card-text">{jewelryData.desciption}</p>;
-            <p className="card-text">{jewelryData.price}</p>;  
+            <h5 className="card-title">{jewelryData.name}</h5>
+            <p className="card-text">{jewelryData.desciption}</p>
+            <p className="card-text">{jewelryData.price}</p>
             <button className="btn btn-warning"
-            onClick={() => addToCart(jewelryData)}>Add to Cart</button>;
+            onClick={() => addToCart(jewelryData)}>Add to Cart</button>
             </div>
         </div>
-    )
-}
+    );
+};
+
+export default JewelryCard;
